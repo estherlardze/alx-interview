5e12e7671b1ace32b495515372f7f7efef6bf802
Esther <99372812+estherlardze@users.noreply.github.com>
2023-02-16 08:09:47 +0000

    Update 0-rotate_2d_matrix.py

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 
-rotated_matrix = [] # Initialize a new matrix
-
-def rotate_2d_matrix(matrix):
-  rotated_matrix = list(zip(*matrix[::-1]))
-  
-if __name__ == "__main__":
-    matrix = [[1, 2, 3],
-              [4, 5, 6],
-              [7, 8, 9]]
-
-    rotate_2d_matrix(matrix)
-    print(matrix)
-
+ def rotate_2d_matrix(self, matrix: List[List[int]]) -> None:
+        def transpose(A):
+            R, C = len(A), len(A[0])
+            T = [[None] * R for _ in range(C)]
+            for r in range(R):
+                for c in range(C):
+                    T[c][r] = A[r][c]
+            return T       
+        
+        matrix[:] = transpose(matrix)
+        for r in range(len(matrix)):
+            matrix[r] = matrix[r][::-1]
